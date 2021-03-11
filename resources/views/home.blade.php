f474aba8de2eec043b2b4987b18b0edeb3106f79
dev-diop221 <diopmouhamed290@gmail.com.com>
2021-03-11 16:49:01 +0000

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Salut {{ auth()->user()->name }}</h1>
+    @foreach (auth()->user()->likes  as $like)
+        {{ $like->title }}
+    @endforeach
+@endsection
